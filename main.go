db71bb3b6e9822274385cdaf1cfac1c1b9d66e76
damingerdai <mingguobin@live.com>
2021-05-18 22:49:30 +0800

    refactor: move buld and yellowWithBG function to colors go fiile

--- main.go
+++ main.go
@@ -26,14 +26,6 @@ func main() {
 	}
 }
 
-func blue(str string) string {
-	return "\033[1;34m" + str + "\033[0m"
-}
-
-func yellowWithBlueBG(str string) string {
-	return "\033[1;33;44m" + str + "\033[0m"
-}
-
 func showPrompt() {
 	u, _ := user.Current()
 	host, _ := os.Hostname()
